f38c9090d2d33d4835653f9c787a6aebfe527eeb
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-08 16:39:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.utils import to_categorical
