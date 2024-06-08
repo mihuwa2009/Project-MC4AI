71d3b6d4239d81faa812cf7c294e539c0a9c7f5e
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-08 16:48:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,14 @@ with tabs[0]:
         st.line_chart(history.history['val_accuracy'], use_container_width=True)
         st.line_chart(history.history['loss'], use_container_width=True)
         st.line_chart(history.history['val_loss'], use_container_width=True)
-    canvas_result = st_canvas(stroke_width=15,
-						      stroke_color='rgb(255, 255, 255)',
-						      background_color='rgb(0, 0, 0)',
-						      height=150,
-						      width=150,
-						      key="canvas")
+with tabs[1]:
+	canvas_result = st_canvas(stroke_width=15,
+				stroke_color='rgb(255, 255, 255)',
+				background_color='rgb(0, 0, 0)',
+				height=150,
+				width=150,
+				key="canvas")
 
-        if canvas_result.image_data is not None:
-          img = canvas_result.image_data
-          st.write(img.shape)
+	if canvas_result.image_data is not None:
+  		img = canvas_result.image_data
+  		st.write(img.shape)
