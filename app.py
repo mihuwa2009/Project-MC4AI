5d3e737f2fcc659b5609093515b602883fd35291
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-08 17:12:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,24 @@ st.title('Handwriting Alphabet Training')
 tabs = st.tabs(["Model Training", "Drawable Canvas"])
 
 with tabs[0]:
-    st.header("Model Training")
-    epochs = st.slider('Number of Epochs', 1, 50, 10)
-    test_size = st.slider('Test Size', 0.1, 0.5, 0.2)
-    samples_per_class = st.slider('Samples per Class', 100, 10000, 1000)
-
-    if st.button('Train Model'):
-        model, history = train_model(epochs, test_size, samples_per_class)
+    	st.header("Model Training")
+    	epochs = st.slider('Number of Epochs', 1, 50, 10)
+    	test_size = st.slider('Test Size', 0.1, 0.5, 0.2)
+    	samples_per_class = st.slider('Samples per Class', 100, 10000, 1000)
+	
+	if st.checkbox('View Data'):
+		
+	
+    	if st.button('Train Model'):
+        	model, history = train_model(epochs, test_size, samples_per_class)
         
-        st.write(f"Final Training Accuracy: {history.history['accuracy'][-1]:.4f}")
-        st.write(f"Final Validation Accuracy: {history.history['val_accuracy'][-1]:.4f}")
+        	st.write(f"Final Training Accuracy: {history.history['accuracy'][-1]:.4f}")
+        	st.write(f"Final Validation Accuracy: {history.history['val_accuracy'][-1]:.4f}")
         
-        st.line_chart(history.history['accuracy'], use_container_width=True)
-        st.line_chart(history.history['val_accuracy'], use_container_width=True)
-        st.line_chart(history.history['loss'], use_container_width=True)
-        st.line_chart(history.history['val_loss'], use_container_width=True)
+        	st.line_chart(history.history['accuracy'], use_container_width=True)
+        	st.line_chart(history.history['val_accuracy'], use_container_width=True)
+        	st.line_chart(history.history['loss'], use_container_width=True)
+        	st.line_chart(history.history['val_loss'], use_container_width=True)
 with tabs[1]:
 	st.header("Draw")
 	canvas_result = st_canvas(stroke_width=15,
